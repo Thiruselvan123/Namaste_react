bbec95f9bda4b93d923e4d04676c168782bdc8ea
Thiruselvan123 <123719355+Thiruselvan123@users.noreply.github.com>
2025-07-31 02:18:01 +0530

    login / logout is added by useState Hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
-const Header = () => (
-  <div className="header">
-    <div className="logo-container">
-      <img src={LOGO_URL} className="logo"></img>
-    </div>
+const Header = () => {
+  const [loginButton, setloginButton] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={LOGO_URL} className="logo"></img>
+      </div>
 
-    <div className="nav-items">
-      <ul>Home</ul>
-      <ul>Sign in</ul>
-      <ul>Cart</ul>
+      <div className="nav-items">
+        <ul>Home</ul>
+        <ul>Sign in</ul>
+        <ul>Cart</ul>
+        <ul>
+          <button
+            className="login-btn"
+            onClick={() => {
+              loginButton == "Login"
+                ? setloginButton("Logout")
+                : setloginButton("Login");
+            }}
+          >
+            {loginButton}
+          </button>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
